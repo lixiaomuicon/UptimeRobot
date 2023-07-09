6011e6ff75fde630bc483a78f2cb22f9b8fd4e98
李小沐 <122504161+lixiaomuicon@users.noreply.github.com>
2023-07-09 14:44:32 +0800

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,28 @@
+window.Config = {
+
+    // 显示标题
+    SiteName: '李小沐的网站状态',
+
+    // UptimeRobot Api Keys
+    // 支持 Monitor-Specific 和 Read-Only
+    ApiKeys: [
+        'ur2059125-d25e8682812e6f5bdc4bfe8a'
+    ],
+
+    // 日志天数
+    CountDays: 90,
+
+    // 是否显示检测站点的链接
+    ShowLink: true,
+
+    // 导航栏菜单
+    Navi: [{
+            text: 'Homepage',
+            url: 'https://lixiaomu.fun/'
+        },
+        {
+            text: 'GitHub',
+            url: 'https://github.com/yb/uptime-status'
+        },
+    ],
+};
